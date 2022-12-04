be9fa0ebae9aa0ace213bfbfc777db442a9a0eaf
Haris <m.haris8790@gmail.com>
2022-12-05 00:31:31 +0500

    Amd Backend

---
+++ uploads.js
@@ -0,0 +1,27 @@
+const multer = require("multer")
+const storage = multer.diskStorage({
+  //Specify the destination directory where the file needs to be saved
+  destination: function (req, file, cb) {
+    cb(null, "./uploads")
+  },
+  //Specify the name of the file. The date is prefixed to avoid overwriting of files.
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "_" + file.originalname)
+  },
+})
+
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype == "application/vnd.android.package-archive"
+    ) {
+      cb(null, true);
+    } else {
+      cb(null, false);
+      return cb(new Error("INVALID_TYPE"));
+    }
+  },
+});
+
+module.exports = upload
